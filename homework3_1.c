c0b491b143f348ce17b2b469b5f0f89f8ebe5a24
3052309087 <3052309087@qq.com>
2024-10-21 16:56:16 +0800

    更新了`homework3_1.c`文件，添加了随机数种子设定、猜测次数统计及输出。同时更新了生成的可执行文件和目标文件

--- homework3_1.c
+++ homework3_1.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <time.h>
 int play()
 {
-    while (1)
-    {
     int a = rand ()% 100;//生成100以内的随机数
     //int a = 10;
+    int c = 1;
+    while (1)
+    {
     int b;
     printf("请猜一个数字");
     scanf("%d", &b);
     if (a == b)
     {
         printf("恭喜你猜对了！");
+        printf("你总共猜了%d次", c);
         system("pause");
         return 0;
     }
     else if(b>a){
         printf("太大了！\n");
+        c++;
     }
     else if(b<a){
         printf("太小了！\n");
+        c++;
     }
     }
     
@@ -31,6 +36,7 @@ int main()
 {
     while (1)
     {
+        srand(time(NULL));//随机数种子
         int a;
         printf("1.play \n");
         printf("2.exit \n");
